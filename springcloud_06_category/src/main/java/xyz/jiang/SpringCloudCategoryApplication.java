7adcf7554c4378ed2c2ea544d35a935081db4ddc
JiangKaiqiang <18369564020@163.com>
2022-06-20 18:05:21 +0800

    openfeign

---
+++ springcloud_06_category/src/main/java/xyz/jiang/SpringCloudCategoryApplication.java
@@ -0,0 +1,14 @@
+package xyz.jiang;
+
+import org.omg.CORBA.ARG_IN;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudCategoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCategoryApplication.class, args);
+    }
+}
